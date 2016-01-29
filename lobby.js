4d1645be3051508279c4c371ce93de1a7ab40072
Simon Scarfe <simon.scarfe@gmail.com>
2016-01-29 00:45:55 +0000

    beginnings of an extension. Pins list of online friends.

---
+++ lobby.js
@@ -0,0 +1,61 @@
+var shitList = ['Gray', 'mewmew', 'SillySod', 'bblum', 'peacho'];  // get shitList
+var friends = ['Draz', 'cursor', 'robotgoblin', 'wizard']; // get friends
+
+var observer = new MutationObserver(function() {
+    getAllPlayers()
+        .filter((player) => shitList.indexOf(player.innerText) !== -1)
+        .forEach((player) => player.classList.add('shit-list'));
+
+    var pinned = getOrCreatePinned();
+    pinned.innerHTML = '';
+
+    var onlineFriends = getAllPlayers()
+        .filter((player) => friends.indexOf(player.innerText) !== -1);
+
+
+    onlineFriends.forEach(function (player, i) {
+        var target = 'friend-' + i;
+
+        Array.from(document.querySelectorAll('#' + target)).forEach(function (existing) {
+            existing.id = '';
+        });
+
+        var gameLine = player.closest('.gameline');
+        gameLine.id = target;
+
+        var li = document.createElement('li');
+        li.innerHTML = '<a href="#' + target + '">' + player.innerText + '</a>';
+
+        pinned.appendChild(li);
+    });
+});
+
+var gameList = document.querySelector('.game-list');
+observer.observe(gameList, {childList: true});
+
+function getAllPlayers () {
+    return Array.from(
+        document.querySelectorAll('.player span:not(.side)')
+    );
+}
+
+function getOrCreatePinned () {
+    var pinnedArea = document.getElementById('pinnedArea');
+
+    if (pinnedArea) {
+        return pinnedArea;
+    }
+    var pinnedContainer = document.createElement('div');
+    pinnedContainer.className = 'pinned-container';
+
+    pinnedContainer.innerHTML = '<h4 class="friends-title">Friends playing right now:</h4>';
+
+    pinnedArea = document.createElement('ul');
+    pinnedArea.id = "pinnedArea";
+
+    pinnedContainer.appendChild(pinnedArea);
+
+    gameList.parentNode.insertBefore(pinnedContainer, gameList);
+
+    return pinnedArea;
+}
