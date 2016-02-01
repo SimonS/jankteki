4064db1574176ff43f5268611c97940fe7f90150
Simon Scarfe <simon.scarfe@gmail.com>
2016-02-01 22:56:58 +0000

    little bit of tidying up

--- eventPage.js
+++ eventPage.js
@@ -1,8 +1,9 @@
 // Show popup icon
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.action === "show") {
-        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.pageAction.show(tabs[0].id);
-        });
+        chrome.tabs.query(
+            {active: true, currentWindow: true},
+            (tabs) => chrome.pageAction.show(tabs[0].id)
+        );
     }
 });
