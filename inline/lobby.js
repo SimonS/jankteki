ed46518b974eb175898831a015ecf226fb7d0d1b
Simon Scarfe <simon.scarfe@gmail.com>
2016-02-07 16:01:49 +0000

    took out flicker fix, applied lobby watcher to all pages

--- inline/lobby.js
+++ inline/lobby.js
@@ -2,42 +2,34 @@ var friends = [];
 
 var observer = new MutationObserver(function() {
     if (document.querySelector('#gamelobby').style.display !== "none") {
-        var oldFriends = [];
-
         chrome.storage.sync.get(['friends'], function (items) {
             var pinned = getOrCreatePinned();
             var friends = items.friends || [],
                 onlineFriends = getAllPlayers()
                     .filter((player) => friends.indexOf(player.innerText) !== -1);
 
-            if (JSON.stringify(onlineFriends) !== JSON.stringify(oldFriends)) {
-                pinned.innerHTML = '';
+            pinned.innerHTML = '';
+            Array.from(document.querySelectorAll('.gameline'))
+                .forEach((game) => game.id = '');
 
-                Array.from(document.querySelectorAll('.gameline'))
-                    .forEach((game) => game.id = '');
+            onlineFriends.forEach(function (player, i) {
+                var target = 'friend-' + i;
 
-                onlineFriends.forEach(function (player, i) {
-                    var target = 'friend-' + i;
+                var gameLine = player.closest('.gameline');
+                gameLine.id = target;
 
-                    var gameLine = player.closest('.gameline');
-                    gameLine.id = target;
+                var li = document.createElement('li');
+                li.innerHTML = '<a href="#' + target + '">' + player.innerText + '</a>';
 
-                    var li = document.createElement('li');
-                    li.innerHTML = '<a href="#' + target + '">' + player.innerText + '</a>';
-
-                    pinned.appendChild(li);
-                });
-            }
+                pinned.appendChild(li);
+            });
 
             if (onlineFriends.length === 0) {
-                pinned.innerHTML = '';
                 var li = document.createElement('li');
                 li.innerHTML = 'Nobody is playing.<br />◉︵◉';
 
                 pinned.appendChild(li);
             }
-
-            oldFriends = onlineFriends;
         });
     }
 });
