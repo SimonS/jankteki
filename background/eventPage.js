32ecb82657b9792c58238bb4dd61185d156b7402
Simon Scarfe <simon.scarfe@gmail.com>
2016-02-07 00:42:47 +0000

    added context menus

--- background/eventPage.js
+++ background/eventPage.js
@@ -10,3 +10,34 @@ chrome.runtime.onInstalled.addListener(function () {
         }]);
     });
 });
+
+chrome.contextMenus.create({
+    title: "Add friend: %s",
+    contexts:["selection"],
+    id: "addFriendContext"
+});
+
+chrome.contextMenus.onClicked.addListener(function(info) {
+    if (info.menuItemId === "addFriendContext") {
+        chrome.storage.sync.get(['friends'], function (items) {
+            var newFriend = info.selectionText,
+                friends = items.friends || [];
+
+            if (newFriend.trim() !== '') {
+                friends.push(newFriend);
+                chrome.storage.sync.set({'friends': friends}, function () {
+                    var notificationOption = {
+                        type: 'basic',
+                        title: 'Friend added',
+                        message: newFriend + ' added as a friend',
+                        iconUrl: 'icons/icon128.png'
+                    };
+
+                    chrome.notifications.create(
+                        'friendAdded', notificationOption, function () {}
+                    );
+                });
+            }
+        });
+    }
+});
