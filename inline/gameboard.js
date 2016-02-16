b87acdc309cb758067b521a9124dba806d8bd1af
Simon Scarfe <simon.scarfe@gmail.com>
2016-02-16 23:54:17 +0000

    working /close-prompt

--- inline/gameboard.js
+++ inline/gameboard.js
@@ -5,32 +5,16 @@ var observer = new MutationObserver(function () {
         createFixesPanel();
 
         var fixesPanel = document.querySelector('#fixes-pane .panel'),
-            closeBtn = document.createElement('button');
-
-        closeBtn.innerHTML = 'Close Prompt';
+            closeBtn = createCloseButton();
 
         fixesPanel.appendChild(closeBtn);
-
-        closeBtn.addEventListener('click', function () {
-            socket.emit("netrunner", {
-                "action": "do",
-                "gameid": localStorage['gameid'],
-                "command": "say",
-                "args": {
-                    "user": user, "text": "Close Prompt"
-                }
-            });
-        });
-
-        // close button: close-prompt
-        //      send command /close-prompt
     }
 });
 
 var gameBoard = document.querySelector('#gameboard');
 observer.observe(gameBoard, {childList: true});
 
-function createFixesPanel() {
+function createFixesPanel () {
     if (!document.getElementById('fixes-pane')) {
         var fixes = document.createElement('div');
 
@@ -42,4 +26,29 @@ function createFixesPanel() {
 
         secondaryPane.insertBefore(fixes, buttonPane);
     }
+}
+
+function createCloseButton () {
+    var button = document.createElement('button');
+
+    button.innerHTML = 'Close Prompt';
+
+    button.addEventListener('click', function () {
+        socket.emit("netrunner", {
+            "action": "do",
+            "gameid": localStorage['gameid'],
+            "command": "say",
+            "side": getSide(),
+            "args": {
+                "user": user,
+                "text": "/close-prompt"
+            }
+        });
+    });
+
+    return button;
+}
+
+function getSide () {
+    return document.querySelector('.runner-board.opponent') ? 'corp' : 'runner';
 }
