64e04f70d180ba3f1efcda8576d2c8c2de573eec
laurie <laurie@brandwatch.com>
2017-12-04 09:56:54 +0000

    Add new option for widen hq. Add class to body if user has option turned on.

--- inline/options.js
+++ inline/options.js
@@ -2,4 +2,8 @@ loadOptions().then(function (options) {
     if (options && options.horizontalScrolling) {
         document.body.classList.add('horizontal-scrolling');
     }
+
+    if (options && options.widenHq) {
+      document.body.classList.add('widen-hq');
+    }
 });
