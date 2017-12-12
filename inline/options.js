28699cd2d5186721be611e8b489035969d112a0b
SimonS <simon.scarfe@gmail.com>
2017-12-12 19:08:26 +0000

    remove horizontal scrolling front-end

--- inline/options.js
+++ inline/options.js
@@ -1,8 +1,4 @@
 loadOptions().then(function (options) {
-    if (options && options.horizontalScrolling) {
-        document.body.classList.add('horizontal-scrolling');
-    }
-
     if (options && options.widenHq) {
       document.body.classList.add('widen-hq');
     }
